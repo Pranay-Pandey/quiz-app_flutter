a095c9f1585d557c26959db362c0f0460695e8ef
Pranay Pandey <pranaypandeyofficial@gmail.com>
2023-09-03 13:57:52 +0530

    Add colored result option

--- lib/response.dart
+++ lib/response.dart
@@ -7,25 +7,39 @@ class Response extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: results.map((e) {
-        return Row(
-          children: [
-            Text(e['id'].toString()),
-            Expanded(
-              child: Column(
-                children: [
-                  Text(e['question'].toString()),
-                  const SizedBox(height: 10),
-                  Text(e['answer'].toString()),
-                  const SizedBox(height: 10),
-                  Text(e['response'].toString()),
-                ],
-              ),
-            )
-          ],
-        );
-      }).toList(),
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: results.map((e) {
+            return Row(
+              children: [
+                SizedBox(
+                  width: 50,
+                  child: Text(
+                    e['id'].toString(),
+                    style:  TextStyle(
+                        color: Colors.white,
+                        backgroundColor: (e['answer'].toString()==e['response'].toString())?Colors.blue:Colors.green,
+                        ),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text(e['question'].toString()),
+                      const SizedBox(height: 10),
+                      Text(e['answer'].toString()),
+                      const SizedBox(height: 10),
+                      Text(e['response'].toString()),
+                    ],
+                  ),
+                )
+              ],
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
